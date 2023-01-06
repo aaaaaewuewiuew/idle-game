fe79478954e2f24dfae26cb233f8712bf318a476
Vesp <115669662+aaaaaewuewiuew@users.noreply.github.com>
2023-01-06 13:26:26 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,10 +1,10 @@
 var score = 1; // Create a variable to store the score in
 
 var title = new Title('Welcome to the Idle Randomness'); // Create a title element
-var button = new Button('Quack', btnPress);   // Create a button element
-var hidden = new Button('Quack 2');
+var button = new Button('Edit', btnPress);   // Create a button element
+var hidden = new Button('Upload');
 //hidden.hide();
-var scr = new Text(score);                    // Create a text element
+var scr = new Text(score);                  // Create a text element
 //changes
 setInterval(btnPress, 1000);   // Set up a loop
 
